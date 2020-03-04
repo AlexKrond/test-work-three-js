3a0df5fcf8b3543cc640286e994a01b2cd30adbb
Alex Kashko <alexkrond@yahoo.com>
2020-03-05 00:34:46 +0300

    Change from inner width/height to client

--- src/handleMouseMove.js
+++ src/handleMouseMove.js
@@ -1,7 +1,7 @@
 const handleMouseMove = ({ mouse, domElement }) => {
   const onMouseMove = event => {
-    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+    mouse.x = (event.clientX / document.documentElement.clientWidth) * 2 - 1;
+    mouse.y = -(event.clientY / document.documentElement.clientHeight) * 2 + 1;
   };
 
   domElement.addEventListener('mousemove', onMouseMove, false);
