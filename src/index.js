ceaf156d8d3f0ef600ce956cc0547c9541f8539b
Alex Kashko <alexkrond@yahoo.com>
2020-03-04 23:50:34 +0300

    Delete timeStamp

--- src/index.js
+++ src/index.js
@@ -4,14 +4,7 @@ import App from "./App.js"
 const app = new App();
 requestAnimationFrame(update);
 
-let lastTime = 0;
-
-function update(timeStamp) {
-  const deltaTime = (timeStamp - lastTime) / 1000; // TODO: delete timeStamp if it won't be use
-  lastTime = timeStamp;
-
+function update() {
   app.render();
-  app.update(deltaTime);
-
   requestAnimationFrame(update);
 }
