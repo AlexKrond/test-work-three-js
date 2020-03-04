5b025765e9aef37b40f886a3fe885488211b5d11
Alex Kashko <alexkrond@yahoo.com>
2020-03-04 23:50:56 +0300

    Refactoring

--- src/handleMouseMove.js
+++ src/handleMouseMove.js
@@ -1,10 +1,10 @@
-const handleMouseMove = mouse => {
+const handleMouseMove = ({ mouse, domElement }) => {
   const onMouseMove = event => {
-    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
   };
 
-  window.addEventListener( 'mousemove', onMouseMove, false );
+  domElement.addEventListener('mousemove', onMouseMove, false);
 };
 
 export default handleMouseMove;
