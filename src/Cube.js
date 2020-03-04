8dfff88c6742546293cbab4765e46db592a7d4c1
Alex Kashko <alexkrond@yahoo.com>
2020-03-04 23:52:36 +0300

    Add edges and color edges function

--- src/Cube.js
+++ src/Cube.js
@@ -1,26 +1,67 @@
 import * as THREE from 'three';
+import config from "./config.js";
 
 
 class Cube {
   constructor(size = 5, position = new THREE.Vector3(0, 0, 0)) {
-    const sphereRadius = 5; // TODO: config file
+    const { sphereRadius, sphereName, edgeWidth } = config;
 
     this.mesh = new THREE.Object3D();
     const cubeGeometry = new THREE.BoxGeometry(size, size, size);
 
-    console.log(cubeGeometry);
+    const spheres = [];
+    const edges = [];
+    const pairs = [[0, 1], [1, 3], [3, 2], [2, 0], [2, 7], [7, 5], [5, 0], [1, 4], [4, 6], [6, 3], [5, 4], [6, 7]];
+
 
     for (let i = 0; i < 8; i++) {
       const material = new THREE.MeshPhongMaterial({
         color: Math.random() * 0xffffff,
         flatShading: true
       });
-      const geometry = new THREE.SphereGeometry(sphereRadius);
-      const sphere = new THREE.Mesh(geometry, material);
-      sphere.position.add(cubeGeometry.vertices[i]);
 
+      const geometry = new THREE.SphereGeometry(sphereRadius, 14, 14);
+      const sphere = new THREE.Mesh(geometry, material);
+
+      sphere.position.add(cubeGeometry.vertices[i]);
+      sphere.name = sphereName;
+
+      spheres.push(sphere);
       this.mesh.add(sphere);
     }
+
+
+    for (let i = 0; i < 12; i++) {
+      const material = new THREE.LineBasicMaterial({
+        color: 0x000000,
+        linewidth: edgeWidth
+      });
+
+      const point1 = spheres[pairs[i][0]].position;
+      const point2 = spheres[pairs[i][1]].position;
+
+      const geometry = new THREE.BufferGeometry().setFromPoints([point1, point2]);
+      const line = new THREE.Line(geometry, material);
+
+      edges.push(line);
+      this.mesh.add(line);
+    }
+
+
+    spheres.forEach((sphere, i) => {
+      sphere.edges = [];
+
+      pairs.forEach((pair, j) => {
+        if (pair.includes(i)) {
+          sphere.edges.push(edges[j]);
+        }
+      });
+
+      sphere.colorEdges = () => {
+        sphere.edges.forEach(edge => edge.material.color.set(sphere.material.color));
+      };
+    });
+
 
     this.mesh.position.add(position);
   }
